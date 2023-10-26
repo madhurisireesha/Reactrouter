093b386619ccde944f608f1f0200ae1ca8dcdf1c
siri <madhurisireesha6@gmail.com>
2023-10-26 23:08:03 +0530

    modify

---
+++ src/components/Nopagefound.js
@@ -0,0 +1,10 @@
+import './nopagefound.css'
+const Nopagefound=()=>{
+    return(
+        <div className="nopage">
+            <img src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png" alt="nopage"/>
+            <h1>Not Found</h1>
+        </div>
+    )
+}
+export default Nopagefound
