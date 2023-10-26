16eabc77ed8f0f39e61d5eef9f28ff55b3ea79f0
siri <madhurisireesha6@gmail.com>
2023-10-26 23:06:20 +0530

    modify

---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+import './contact.css'
+const Contact=()=>{
+    return(
+        <div className='contactcontainer'>
+            <img src="https://assets.ccbp.in/frontend/react-js/contact-blog-img.png" alt="contact" className='contact'/>
+            <h1>Contact</h1>
+        </div>
+    )
+}
+export default Contact
