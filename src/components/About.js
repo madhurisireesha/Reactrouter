107a156a66d5823be79d1dfca614bf55287aeaef
siri <madhurisireesha6@gmail.com>
2023-10-26 23:05:41 +0530

    modify

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import './about.css'
+const About=()=>{
+    return(
+        <div className="aboutcontainer">
+            <img src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png " alt="contact" className="contact"/>
+            <h1>About</h1>
+            <h3>I love to create! I am a frontend developer</h3>
+        </div>
+    )
+}
+export default About
