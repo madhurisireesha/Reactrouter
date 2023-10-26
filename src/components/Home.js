f247a2da26dc39c2601ff0906149f1f5f035cf7f
siri <madhurisireesha6@gmail.com>
2023-10-26 23:07:30 +0530

    modify

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import './home.css'
+const Home=()=>{
+    return(
+        <div className="homecontainer">
+            <img src="https://assets.ccbp.in/frontend/react-js/home-blog-img.png " alt="home" className="home"/>
+            <h1>Home</h1>
+        </div>
+    )
+}
+export default Home
